976554f4e557bfe9f979a15e267a98aae0b88ee5
Eva <eva.coka04@gmail.com>
2026-08-18 01:09:21 +0200

    Add navigation menu
    Created Menu.jsx and added it to AppRouter.jsx.

--- ski-resort-app/src/routes/AppRouter.jsx
+++ ski-resort-app/src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function AppRouter() {
   return (
     <BrowserRouter>
+    <Menu />
       <Routes>
         <Route path="/" element={<h1>Ski Resort App</h1>} />
       </Routes>
